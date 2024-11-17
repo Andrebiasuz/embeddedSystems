f68e9d52255ad85e4703d40187c4cfe3dac7ae84
André Biasuz <andrebiasuz@gmail.com>
2024-11-17 12:19:48 +0100

    Digital input test with pull down resistor

--- ESP32_Related/P1_HelloWorld/main/main.c
+++ ESP32_Related/P1_HelloWorld/main/main.c
@@ -6,22 +6,30 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
-#define PIN_USED 5
-
+#define PIN_USED 25
+#define PIN_DI 34
 
 void app_main(void)
 {
     gpio_pad_select_gpio(PIN_USED);
     gpio_set_direction(PIN_USED, GPIO_MODE_OUTPUT);
 
+    gpio_pad_select_gpio(PIN_DI);
+    gpio_set_direction(PIN_DI, GPIO_MODE_INPUT);
+
     bool status = 0;
 
     while(1){
         
-        status = !status;
+        status = gpio_get_level(PIN_DI);
+        if (status == 1) {
+            ESP_LOGW("GPIO 34", "PIN STATUS CHANGED");
+        }
+
         gpio_set_level(PIN_USED,status);
+        //ESP_LOGW("GPIO 5", "PIN STATUS CHANGED");
+
         vTaskDelay(10 / portTICK_PERIOD_MS);     
-        ESP_LOGW("GPIO 5", "PIN STATUS CHANGED");
 
     }
 }
