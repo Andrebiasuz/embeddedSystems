980d1c0d0896a55b2cf1e83e2cbb84d59b201085
André Biasuz <andrebiasuz@gmail.com>
2024-11-18 14:05:12 +0100

    Digital input test, with LED as System running

--- ESP32_Related/P1_HelloWorld/main/main.c
+++ ESP32_Related/P1_HelloWorld/main/main.c
@@ -6,29 +6,43 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
-#define PIN_USED 25
-#define PIN_DI 34
+#define PIN_POWERON 13 // Intermitent blink, showing system is running
+#define PIN_DO 25 // Led Activated with Microswitch
+#define PIN_DI 34 // Microswitch connected
 
 void app_main(void)
 {
-    gpio_pad_select_gpio(PIN_USED);
-    gpio_set_direction(PIN_USED, GPIO_MODE_OUTPUT);
 
     gpio_pad_select_gpio(PIN_DI);
     gpio_set_direction(PIN_DI, GPIO_MODE_INPUT);
 
-    bool status = 0;
+    gpio_pad_select_gpio(PIN_DO);
+    gpio_set_direction(PIN_DO, GPIO_MODE_OUTPUT);
+
+    gpio_pad_select_gpio(PIN_POWERON);
+    gpio_set_direction(PIN_POWERON, GPIO_MODE_OUTPUT);
+
+    bool state_DI_PIN = 0;
 
     while(1){
+
+        // LED For Power ON 
+        gpio_set_level(PIN_POWERON,1);
         
-        status = gpio_get_level(PIN_DI);
-        if (status == 1) {
+
+        // Read DI, Activate DO routine
+
+        state_DI_PIN = gpio_get_level(PIN_DI);
+
+        if (state_DI_PIN == 1) {
             ESP_LOGW("GPIO 34", "PIN STATUS CHANGED");
+            gpio_set_level(PIN_DO,state_DI_PIN);
+        }
+        else {
+            gpio_set_level(PIN_DO,0);
         }
 
-        gpio_set_level(PIN_USED,status);
-        //ESP_LOGW("GPIO 5", "PIN STATUS CHANGED");
-
+        // Delay for RTOS
         vTaskDelay(10 / portTICK_PERIOD_MS);     
 
     }
